a7c66ea4fe698a4f3a6cb0d4136901546b7cf00d
inventor-bs <hongduc6701@gmail.com>
2022-08-12 00:49:46 +0700

    Finish First Folder Exercise

--- Bai3/index.js
+++ Bai3/index.js
@@ -17,14 +17,19 @@ let closeIcon = document.querySelector('.form_Login__icon i');
 let loginForm = document.querySelector('.wrapper');
 let loading = document.querySelector('.form_Login__loading');
 let buttonLogin = document.querySelector('.form_Login__button');
+let textErrorUser = document.querySelector('.form_Login__input__errorUsername');
+let textErrorPassword = document.querySelector('.form_Login__input__errorPassword');
 
 
-let textUser = document.getElementById('username');
-let textPassword = document.getElementById('password');
+
 
 let toogleLogin = () => {
     loading.classList.toggle('hide');
     buttonLogin.classList.toggle('hide');
+    setTimeout(() => {
+        buttonLogin.classList.toggle('hide');
+        loading.classList.toggle('hide');
+    }, 1500)
 }
 
 
@@ -34,12 +39,46 @@ let toogleForm = () => {
 
 buttonOpenForm.addEventListener('click', toogleForm);
 closeIcon.addEventListener('click', toogleForm);
-buttonLogin.addEventListener('click', toogleLogin);
-loading.addEventListener('click', toogleLogin);
+
+buttonLogin.addEventListener('click', () => {
+    toogleLogin();
+    let textUser = document.getElementById('username').value;
+    let textPassword = document.getElementById('password').value;
+    textErrorUser.innerHTML = '';
+    textErrorPassword.innerHTML = '';
+    console.log({ textUser, textPassword });
+    if (textUser === '' && textPassword === '') {
+        textErrorUser.innerHTML = 'Bạn chưa nhập Username';
+        textErrorPassword.innerHTML = 'Bạn chưa nhập Password';
+        return;
+    }
+    if (textUser === '') {
+        textErrorUser.innerHTML = 'Bạn chưa nhập Username';
+        return;
+    }
+    if (textPassword === '') {
+        textErrorPassword.innerHTML = 'Bạn chưa nhập Password';
+        return;
+    }
+    if (username !== textUser && password !== textPassword) {
+        textErrorPassword.innerHTML = 'Tài khoản hoặc mật khẩu không đúng';
+        return;
+    }
+    if (username === textUser && password === textPassword) {
+        document.getElementById('username').value = '';
+        document.getElementById('password').value = '';
+        buttonOpenForm.innerHTML = 'Đăng nhập thành công';
+        toogleForm();
+        toogleLogin();
+    }
+});
+
+// loading.addEventListener('click', () => {
+//     toogleLogin();
+//     textErrorUser.innerHTML = '';
+//     textErrorPassword.innerHTML = '';
+// });
 
 
 
 
-if (username !== textUser && password !== textPassword) {
-
-}
--- Bai4/index.js
+++ Bai4/index.js
@@ -1,3 +1,123 @@
-for (var i = 0; i < 10; i++) {
-    console.log(i);
+
+/** 
+ *  Biến user and password
+ */
+
+let username = 'admin';
+let password = 'admin';
+let newpassword = '';
+
+let btnLogin = document.querySelector('.btn__Login');
+
+let errorUser = document.getElementById('errorUsername');
+let errorPassword = document.getElementById('errorPassword');
+
+
+let textUser = document.getElementById('username');
+let textPassword = document.getElementById('password');
+
+const clear = () => {
+    errorUser.innerHTML = '';
+    errorPassword.innerHTML = '';
+    document.getElementById('username').value = '';
+    document.getElementById('password').value = '';
 }
+
+const validate = () => {
+    const textUser = document.getElementById('username').value;
+    const textPassword = document.getElementById('password').value;
+    let state = document.querySelector('#changeState').innerHTML;
+    if (state === 'Forgot Password?') {
+        if (textUser.trim().length === 0 && textPassword.trim().length === 0) {
+            errorUser.innerHTML = `Bạn chưa nhập Username`;
+            errorPassword.innerHTML = `Bạn chưa nhập Password`;
+            return;
+        }
+        if (textUser.trim().length === 0) {
+            errorUser.innerHTML = `Bạn chưa nhập Username`;
+            return;
+        }
+        if (textPassword.trim().length === 0) {
+            errorPassword.innerHTML = `Bạn chưa nhập Password`;
+            return;
+        }
+        if (textUser !== username || textPassword !== password) {
+            errorPassword.innerHTML = `Bạn nhập Username hoặc Password sai!`;
+            return;
+        }
+        if (textUser === username || textPassword === password) {
+            console.log(document.getElementById('info'));
+            document.getElementById('info').innerHTML = 'Đăng nhập thành công';
+            console.log(document.getElementById('info'));
+            document.getElementById('closeForm').click();
+            setTimeout(() => {
+                document.getElementById('info').innerHTML = 'Đăng nhập hệ thống';
+            }, 5100)
+        }
+    }
+    else {
+        if (textUser.trim().length === 0 && textPassword.trim().length === 0) {
+            errorUser.innerHTML = `Bạn chưa nhập Old Password`;
+            errorPassword.innerHTML = `Bạn chưa nhập New Password`;
+            return;
+        }
+        if (textUser.trim().length === 0) {
+            errorUser.innerHTML = `Bạn chưa nhập Old Password`;
+            return;
+        }
+        if (textPassword.trim().length === 0) {
+            errorPassword.innerHTML = `Bạn chưa nhập New Password`;
+            return;
+        }
+        if (textUser !== password) {
+            errorUser.innerHTML = `Bạn nhập sai Old Password`;
+            return;
+        }
+        if (textUser === password) {
+            newpassword = textPassword;
+            password = newpassword;
+            errorPassword.style.color = 'green';
+            errorPassword.innerHTML = `Đổi mật khẩu thành công`;
+            setTimeout(() => {
+                document.querySelector('#changeState').click();
+            }, 1500);
+        }
+    }
+
+}
+
+btnLogin.addEventListener('click', () => {
+    btnLogin.value = `Loading...`;
+    console.log(password);
+    btnLogin.style.background = '#12be96'
+    setTimeout(() => {
+        btnLogin.value = `Login`;
+        btnLogin.style.background = '#00ce81';
+        clear();
+    }, 1500);
+    validate();
+})
+
+document.querySelector('#changeState').addEventListener('click', () => {
+    let state = document.querySelector('#changeState').innerHTML;
+    if (state === 'Forgot Password?') {
+        clear();
+        document.querySelector('#changeState').innerHTML = 'Back';
+        document.querySelector('.modal-title').innerHTML = 'Forgot Password';
+        textUser.placeholder = 'Old Password';
+        textPassword.placeholder = 'New Password';
+        btnLogin.value = 'Save';
+    }
+    else {
+        document.querySelector('#changeState').innerHTML = 'Forgot Password?';
+        clear();
+        document.querySelector('#changeState').innerHTML = 'Forgot Password?';
+        document.querySelector('.modal-title').innerHTML = 'Member Login';
+        textUser.placeholder = 'Username';
+        textPassword.placeholder = 'Password';
+        btnLogin.value = 'Login';
+    }
+});
+
+
+
